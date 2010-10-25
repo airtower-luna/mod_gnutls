e02dd8c03ce5b66c81a47830eebe73a21ce71002
Nikos Mavrogiannopoulos <nmav@gnutls.org>
2010-10-25 21:21:04 +0200

    indented code

--- src/gnutls_lua.c
+++ src/gnutls_lua.c
@@ -25,268 +25,285 @@
 
 static char *MGS_LUA_RRKEY = "request_rec";
 
-static request_rec *mgs_lua_getrr(lua_State *lvm)
+static request_rec *mgs_lua_getrr(lua_State * lvm)
 {
-    request_rec *r;
-    
-    /* Push the request_rec off the registry, onto the stack. */
-    lua_pushlightuserdata(lvm, MGS_LUA_RRKEY);
-    lua_gettable(lvm, LUA_REGISTRYINDEX);
-    r = lua_touserdata(lvm, -1);
-    lua_pop(lvm, 1); 
-    return r;
+	request_rec *r;
+
+	/* Push the request_rec off the registry, onto the stack. */
+	lua_pushlightuserdata(lvm, MGS_LUA_RRKEY);
+	lua_gettable(lvm, LUA_REGISTRYINDEX);
+	r = lua_touserdata(lvm, -1);
+	lua_pop(lvm, 1);
+	return r;
 }
 
-static int get_request_table(lua_State *lvm, long offset)
-{   
-    const char *key;
-    request_rec *r;
-    const char *value;
-    apr_table_t *t;
-    key = luaL_checkstring(lvm, 1);
-
-    r = mgs_lua_getrr(lvm);
-    
-    t = *(apr_table_t **)((char *)r + offset);
-
-    value = apr_table_get(t, key);
-
-    if (value) {
-        lua_pushstring(lvm, value); 
-        return 1;
-    }
-    else {
-        return 0;
-    }
-}
-
-static int mgs_lua_getenv(lua_State *lvm)
+static int get_request_table(lua_State * lvm, long offset)
 {
-    return get_request_table(lvm, APR_OFFSETOF(request_rec, subprocess_env));
+	const char *key;
+	request_rec *r;
+	const char *value;
+	apr_table_t *t;
+	key = luaL_checkstring(lvm, 1);
+
+	r = mgs_lua_getrr(lvm);
+
+	t = *(apr_table_t **) ((char *) r + offset);
+
+	value = apr_table_get(t, key);
+
+	if (value) {
+		lua_pushstring(lvm, value);
+		return 1;
+	} else {
+		return 0;
+	}
 }
 
-static int mgs_lua_getheader(lua_State *lvm)
-{   
-    return get_request_table(lvm, APR_OFFSETOF(request_rec, headers_in));
-} 
+static int mgs_lua_getenv(lua_State * lvm)
+{
+	return get_request_table(lvm,
+				 APR_OFFSETOF(request_rec,
+					      subprocess_env));
+}
+
+static int mgs_lua_getheader(lua_State * lvm)
+{
+	return get_request_table(lvm,
+				 APR_OFFSETOF(request_rec, headers_in));
+}
 
 static const luaL_reg mgs_lua_reg[] = {
-    {"getenv", mgs_lua_getenv},
-    {"header", mgs_lua_getheader},
-    {NULL, NULL}
+	{"getenv", mgs_lua_getenv},
+	{"header", mgs_lua_getheader},
+	{NULL, NULL}
 };
 
-lua_State* get_luastate() 
+lua_State *get_luastate()
 {
-    lua_State* lvm = lua_open();
-    luaopen_base(lvm);
-    luaopen_io(lvm);
-    luaopen_table(lvm);
-    luaopen_string(lvm);
-    luaopen_math(lvm);
-    luaopen_loadlib(lvm);
-    luaL_openlib(lvm, "ap", mgs_lua_reg, 0);
-    
-    return lvm;
+	lua_State *lvm = lua_open();
+	luaopen_base(lvm);
+	luaopen_io(lvm);
+	luaopen_table(lvm);
+	luaopen_string(lvm);
+	luaopen_math(lvm);
+	luaopen_loadlib(lvm);
+	luaL_openlib(lvm, "ap", mgs_lua_reg, 0);
+
+	return lvm;
 }
 
-int mgs_authz_lua(request_rec* r)
+int mgs_authz_lua(request_rec * r)
 {
-    int rv;
-    lua_State* lvm;
-    mgs_dirconf_rec *dc = ap_get_module_config(r->per_dir_config,
-                                               &gnutls_module);
-    
-    if (dc->lua_bytecode_len <= 0) {
-        return 0;
-    }
-    
-    lvm = get_luastate();
-    lua_pushlightuserdata(lvm, MGS_LUA_RRKEY);
-    lua_pushlightuserdata(lvm, r);
-    lua_settable(lvm, LUA_REGISTRYINDEX);
-    
-    /* Push Bytecode onto the stack */
-    rv = luaL_loadbuffer(lvm, dc->lua_bytecode, dc->lua_bytecode_len, "gnutls-lua");
+	int rv;
+	lua_State *lvm;
+	mgs_dirconf_rec *dc = ap_get_module_config(r->per_dir_config,
+						   &gnutls_module);
 
-    if (rv != 0) {
-        /* Get the Error message */
-        const char* error = lua_tostring(lvm, -1);
-        ap_log_rerror(APLOG_MARK, APLOG_DEBUG, 0, r, 
-                      "GnuTLS: Error Loading Lua Bytecode: %s", error);
-        lua_pop(lvm, 1);
-        return -1;
-    }
-    
-    rv = lua_pcall(lvm, 0, 1, 0);
-    if (rv != 0) {
-        /* Get the Error message */
-        const char* error = lua_tostring(lvm, -1);
-        ap_log_rerror(APLOG_MARK, APLOG_DEBUG, 0, r, 
-                      "GnuTLS: Error Running Lua: %s", error);
-        lua_pop(lvm, 1);
-        return -1;
-    }
-    
-    rv = (int)lua_tonumber(lvm, -1);
-    ap_log_rerror(APLOG_MARK, APLOG_DEBUG, 0, r, 
-                  "GnuTLS: (%d) Lua Return: %d",
-                  dc->lua_bytecode_len, rv);
-    lua_pop(lvm, 1);
-    lua_close(lvm);
-    return rv;
+	if (dc->lua_bytecode_len <= 0) {
+		return 0;
+	}
+
+	lvm = get_luastate();
+	lua_pushlightuserdata(lvm, MGS_LUA_RRKEY);
+	lua_pushlightuserdata(lvm, r);
+	lua_settable(lvm, LUA_REGISTRYINDEX);
+
+	/* Push Bytecode onto the stack */
+	rv = luaL_loadbuffer(lvm, dc->lua_bytecode, dc->lua_bytecode_len,
+			     "gnutls-lua");
+
+	if (rv != 0) {
+		/* Get the Error message */
+		const char *error = lua_tostring(lvm, -1);
+		ap_log_rerror(APLOG_MARK, APLOG_DEBUG, 0, r,
+			      "GnuTLS: Error Loading Lua Bytecode: %s",
+			      error);
+		lua_pop(lvm, 1);
+		return -1;
+	}
+
+	rv = lua_pcall(lvm, 0, 1, 0);
+	if (rv != 0) {
+		/* Get the Error message */
+		const char *error = lua_tostring(lvm, -1);
+		ap_log_rerror(APLOG_MARK, APLOG_DEBUG, 0, r,
+			      "GnuTLS: Error Running Lua: %s", error);
+		lua_pop(lvm, 1);
+		return -1;
+	}
+
+	rv = (int) lua_tonumber(lvm, -1);
+	ap_log_rerror(APLOG_MARK, APLOG_DEBUG, 0, r,
+		      "GnuTLS: (%d) Lua Return: %d",
+		      dc->lua_bytecode_len, rv);
+	lua_pop(lvm, 1);
+	lua_close(lvm);
+	return rv;
 }
 
-static apr_size_t config_getstr(ap_configfile_t *cfg, char *buf, size_t bufsiz)
+static apr_size_t config_getstr(ap_configfile_t * cfg, char *buf,
+				size_t bufsiz)
 {
-    apr_size_t i = 0;
-    
-    if (cfg->getstr) {
-        const char *res = (cfg->getstr)(buf, bufsiz, cfg->param);
-        if (res) {
-            i = strlen(buf);
-            if (i && buf[i - 1] == '\n') ++cfg->line_number;
-        }
-        else {
-            buf[0] = '\0';
-            i = 0;
-        }
-    }
-    else {
-        while (i < bufsiz) {
-            int ch = (cfg->getch)(cfg->param);
-            if (ch == EOF) break;
-            buf[i++] = ch;
-            if (ch == '\n') {
-                ++cfg->line_number;
-                break;
-            }
-        }
-    }
-    return i;
+	apr_size_t i = 0;
+
+	if (cfg->getstr) {
+		const char *res = (cfg->getstr) (buf, bufsiz, cfg->param);
+		if (res) {
+			i = strlen(buf);
+			if (i && buf[i - 1] == '\n')
+				++cfg->line_number;
+		} else {
+			buf[0] = '\0';
+			i = 0;
+		}
+	} else {
+		while (i < bufsiz) {
+			int ch = (cfg->getch) (cfg->param);
+			if (ch == EOF)
+				break;
+			buf[i++] = ch;
+			if (ch == '\n') {
+				++cfg->line_number;
+				break;
+			}
+		}
+	}
+	return i;
 }
 
 struct cr_ctx {
-    ap_configfile_t *cfp;
-    size_t startline;
-    char buf[HUGE_STRING_LEN];
+	ap_configfile_t *cfp;
+	size_t startline;
+	char buf[HUGE_STRING_LEN];
 };
 
 static const char *LUACMD = "gnutlsrequire";
-static const char *lf = "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n";
+static const char *lf =
+    "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n";
 #define N_LF 32
 
-static const char *direct_chunkreader(lua_State *lvm, void *udata, size_t *plen)
+static const char *direct_chunkreader(lua_State * lvm, void *udata,
+				      size_t * plen)
 {
-    const char *p;
-    struct cr_ctx *ctx = udata;
-    
-    if (ctx->startline) {
-        *plen = ctx->startline > N_LF ? N_LF : ctx->startline;
-        ctx->startline -= *plen;
-        return lf;
-    }
-    *plen = config_getstr(ctx->cfp, ctx->buf, HUGE_STRING_LEN);
-    
-    for (p = ctx->buf; isspace(*p); ++p);
-    if (p[0] == '<' && p[1] == '/') {
-        int i = 0;
-        while (i < strlen(LUACMD)) {
-            if (tolower(p[i + 2]) != LUACMD[i]) return ctx->buf;
-            ++i;
-        }
-        *plen = 0;
-        return NULL;
-    }
-    return ctx->buf;
+	const char *p;
+	struct cr_ctx *ctx = udata;
+
+	if (ctx->startline) {
+		*plen = ctx->startline > N_LF ? N_LF : ctx->startline;
+		ctx->startline -= *plen;
+		return lf;
+	}
+	*plen = config_getstr(ctx->cfp, ctx->buf, HUGE_STRING_LEN);
+
+	for (p = ctx->buf; isspace(*p); ++p);
+	if (p[0] == '<' && p[1] == '/') {
+		int i = 0;
+		while (i < strlen(LUACMD)) {
+			if (tolower(p[i + 2]) != LUACMD[i])
+				return ctx->buf;
+			++i;
+		}
+		*plen = 0;
+		return NULL;
+	}
+	return ctx->buf;
 }
 
-static int ldump_writer (lua_State *L, const void* b, size_t size, void* B) {
-    (void)L;
-    luaL_addlstring((luaL_Buffer*) B, (const char *)b, size);
-    return 1;
+static int ldump_writer(lua_State * L, const void *b, size_t size, void *B)
+{
+	(void) L;
+	luaL_addlstring((luaL_Buffer *) B, (const char *) b, size);
+	return 1;
 }
 
 /* a bytecode buffer*/
 typedef struct bcbuf_ctx {
-    apr_size_t buflen;
-    char* buf;
+	apr_size_t buflen;
+	char *buf;
 } bcbuf_ctx;
 
-const char *mgs_set_require_section(cmd_parms *cmd, void *mconfig, const char *arg)
+const char *mgs_set_require_section(cmd_parms * cmd, void *mconfig,
+				    const char *arg)
 {
-    apr_size_t bytecode_len;
-    const char* bytecode;
-    bcbuf_ctx* bcbuf;
-    luaL_Buffer b;
-    ap_directive_t **current = mconfig;
-    struct cr_ctx ctx[1];
-    int result;
-    const char *filename = apr_psprintf(cmd->pool, "@%s", cmd->config_file->name);
-    // get a word argument
-    const char *word;
-    apr_size_t wordlen;
-    lua_State *lvm = get_luastate();
+	apr_size_t bytecode_len;
+	const char *bytecode;
+	bcbuf_ctx *bcbuf;
+	luaL_Buffer b;
+	ap_directive_t **current = mconfig;
+	struct cr_ctx ctx[1];
+	int result;
+	const char *filename =
+	    apr_psprintf(cmd->pool, "@%s", cmd->config_file->name);
+	// get a word argument
+	const char *word;
+	apr_size_t wordlen;
+	lua_State *lvm = get_luastate();
 
-    word = ap_getword_conf(cmd->pool, &arg);
-    wordlen = strlen(word);
-    do {
-        if (wordlen) {
-            if (word[wordlen - 1] == '>') {
-                --wordlen;
-                break;
-            }
-            if (*arg == '>') break;
-        }
-        return apr_pstrcat(cmd->pool, "<", LUACMD, "> takes exactly one argument", NULL);
-    } while (0);
+	word = ap_getword_conf(cmd->pool, &arg);
+	wordlen = strlen(word);
+	do {
+		if (wordlen) {
+			if (word[wordlen - 1] == '>') {
+				--wordlen;
+				break;
+			}
+			if (*arg == '>')
+				break;
+		}
+		return apr_pstrcat(cmd->pool, "<", LUACMD,
+				   "> takes exactly one argument", NULL);
+	} while (0);
 
-    ctx->cfp = cmd->config_file;
-    ctx->startline = cmd->config_file->line_number;
-    lua_settop(lvm, 0);
-    result = lua_load(lvm, direct_chunkreader, ctx, filename);
+	ctx->cfp = cmd->config_file;
+	ctx->startline = cmd->config_file->line_number;
+	lua_settop(lvm, 0);
+	result = lua_load(lvm, direct_chunkreader, ctx, filename);
 
-    if (result != 0) {
-        word = apr_pstrcat(cmd->pool, "Lua Error:", lua_tostring(lvm, -1), NULL);
-        lua_close(lvm);
-        return word;
-    }
-    else {
-        luaL_buffinit(lvm, &b);
-        lua_dump(lvm, ldump_writer, &b);
-        luaL_pushresult(&b);
-        bytecode = lua_tostring(lvm, -1);
-        bytecode_len = lua_strlen(lvm, -1);
-    }
-    
-    /* Here, we have to replace our current config node for the next pass */
-    if (!*current) {
-        *current = apr_pcalloc(cmd->pool, sizeof(**current));
-    }
-    
-    (*current)->filename = cmd->config_file->name;
-    (*current)->line_num = ctx->startline;
-    (*current)->directive = apr_pstrdup(cmd->pool, "GnuTLSRequireByteCode");
-    (*current)->args = NULL;
+	if (result != 0) {
+		word =
+		    apr_pstrcat(cmd->pool, "Lua Error:",
+				lua_tostring(lvm, -1), NULL);
+		lua_close(lvm);
+		return word;
+	} else {
+		luaL_buffinit(lvm, &b);
+		lua_dump(lvm, ldump_writer, &b);
+		luaL_pushresult(&b);
+		bytecode = lua_tostring(lvm, -1);
+		bytecode_len = lua_strlen(lvm, -1);
+	}
 
-    bcbuf = apr_pcalloc(cmd->pool, sizeof(bcbuf));
-    bcbuf->buflen = bytecode_len;
-    bcbuf->buf = apr_pstrmemdup(cmd->pool, bytecode, bytecode_len);
+	/* Here, we have to replace our current config node for the next pass */
+	if (!*current) {
+		*current = apr_pcalloc(cmd->pool, sizeof(**current));
+	}
 
-    (*current)->data = bcbuf;
-    lua_close(lvm);
-    return NULL;
+	(*current)->filename = cmd->config_file->name;
+	(*current)->line_num = ctx->startline;
+	(*current)->directive =
+	    apr_pstrdup(cmd->pool, "GnuTLSRequireByteCode");
+	(*current)->args = NULL;
+
+	bcbuf = apr_pcalloc(cmd->pool, sizeof(bcbuf));
+	bcbuf->buflen = bytecode_len;
+	bcbuf->buf = apr_pstrmemdup(cmd->pool, bytecode, bytecode_len);
+
+	(*current)->data = bcbuf;
+	lua_close(lvm);
+	return NULL;
 }
 
-const char *mgs_set_require_bytecode(cmd_parms *cmd, void *mconfig, const char *arg)
+const char *mgs_set_require_bytecode(cmd_parms * cmd, void *mconfig,
+				     const char *arg)
 {
-    bcbuf_ctx* bcbuf;
-    ap_directive_t *directive = cmd->directive;
-    mgs_dirconf_rec *dc = mconfig;
+	bcbuf_ctx *bcbuf;
+	ap_directive_t *directive = cmd->directive;
+	mgs_dirconf_rec *dc = mconfig;
 
-    bcbuf = directive->data;
-    dc->lua_bytecode_len = bcbuf->buflen;
-    dc->lua_bytecode = apr_pstrmemdup(cmd->pool, bcbuf->buf, bcbuf->buflen);
-    
-    return NULL;
+	bcbuf = directive->data;
+	dc->lua_bytecode_len = bcbuf->buflen;
+	dc->lua_bytecode =
+	    apr_pstrmemdup(cmd->pool, bcbuf->buf, bcbuf->buflen);
+
+	return NULL;
 }
--- src/mod_gnutls.c
+++ src/mod_gnutls.c
@@ -19,143 +19,146 @@
 
 static void gnutls_hooks(apr_pool_t * p)
 {
-    ap_hook_pre_connection(mgs_hook_pre_connection, NULL, NULL,
-                           APR_HOOK_MIDDLE);
-    ap_hook_post_config(mgs_hook_post_config, NULL, NULL,
-                        APR_HOOK_MIDDLE);
-    ap_hook_child_init(mgs_hook_child_init, NULL, NULL,
-                       APR_HOOK_MIDDLE);
+	ap_hook_pre_connection(mgs_hook_pre_connection, NULL, NULL,
+			       APR_HOOK_MIDDLE);
+	ap_hook_post_config(mgs_hook_post_config, NULL, NULL,
+			    APR_HOOK_MIDDLE);
+	ap_hook_child_init(mgs_hook_child_init, NULL, NULL,
+			   APR_HOOK_MIDDLE);
 #if USING_2_1_RECENT
-    ap_hook_http_scheme(mgs_hook_http_scheme, NULL, NULL,
-                        APR_HOOK_MIDDLE);
+	ap_hook_http_scheme(mgs_hook_http_scheme, NULL, NULL,
+			    APR_HOOK_MIDDLE);
 #else
-    ap_hook_http_method(mgs_hook_http_scheme, NULL, NULL,
-                        APR_HOOK_MIDDLE);
+	ap_hook_http_method(mgs_hook_http_scheme, NULL, NULL,
+			    APR_HOOK_MIDDLE);
 #endif
-    ap_hook_default_port(mgs_hook_default_port, NULL, NULL,
-                         APR_HOOK_MIDDLE);
-    ap_hook_pre_config(mgs_hook_pre_config, NULL, NULL,
-                       APR_HOOK_MIDDLE);
-    
-    ap_hook_access_checker(mgs_hook_authz, NULL, NULL, APR_HOOK_REALLY_FIRST);
-    
-    ap_hook_fixups(mgs_hook_fixups, NULL, NULL, APR_HOOK_REALLY_FIRST);
-    
-    /* TODO: HTTP Upgrade Filter */
-    /* ap_register_output_filter ("UPGRADE_FILTER", 
-     *          ssl_io_filter_Upgrade, NULL, AP_FTYPE_PROTOCOL + 5);
-     */
+	ap_hook_default_port(mgs_hook_default_port, NULL, NULL,
+			     APR_HOOK_MIDDLE);
+	ap_hook_pre_config(mgs_hook_pre_config, NULL, NULL,
+			   APR_HOOK_MIDDLE);
 
-    ap_register_input_filter(GNUTLS_INPUT_FILTER_NAME,
-                             mgs_filter_input, NULL,
-                             AP_FTYPE_CONNECTION + 5);
-    ap_register_output_filter(GNUTLS_OUTPUT_FILTER_NAME,
-                              mgs_filter_output, NULL,
-                              AP_FTYPE_CONNECTION + 5);
+	ap_hook_access_checker(mgs_hook_authz, NULL, NULL,
+			       APR_HOOK_REALLY_FIRST);
+
+	ap_hook_fixups(mgs_hook_fixups, NULL, NULL, APR_HOOK_REALLY_FIRST);
+
+	/* TODO: HTTP Upgrade Filter */
+	/* ap_register_output_filter ("UPGRADE_FILTER", 
+	 *          ssl_io_filter_Upgrade, NULL, AP_FTYPE_PROTOCOL + 5);
+	 */
+
+	ap_register_input_filter(GNUTLS_INPUT_FILTER_NAME,
+				 mgs_filter_input, NULL,
+				 AP_FTYPE_CONNECTION + 5);
+	ap_register_output_filter(GNUTLS_OUTPUT_FILTER_NAME,
+				  mgs_filter_output, NULL,
+				  AP_FTYPE_CONNECTION + 5);
 }
 
 static const command_rec mgs_config_cmds[] = {
-    AP_INIT_TAKE1("GnuTLSClientVerify", mgs_set_client_verify,
-                  NULL,
-                  RSRC_CONF|OR_AUTHCFG,
-                  "Set Verification Requirements of the Client Certificate"),
-    AP_INIT_TAKE1("GnuTLSClientCAFile", mgs_set_client_ca_file,
-                  NULL,
-                  RSRC_CONF,
-                  "Set the CA File to verify Client Certificates"),
-    AP_INIT_TAKE1("GnuTLSX509CAFile", mgs_set_client_ca_file,
-                  NULL,
-                  RSRC_CONF,
-                  "Set the CA File to verify Client Certificates"),
-    AP_INIT_TAKE1("GnuTLSPGPKeyringFile", mgs_set_keyring_file,
-                  NULL,
-                  RSRC_CONF,
-                  "Set the Keyring File to verify Client Certificates"),
-    AP_INIT_TAKE1("GnuTLSDHFile", mgs_set_dh_file,
-                  NULL,
-                  RSRC_CONF,
-                  "Set the file to read Diffie Hellman parameters from"),
-    AP_INIT_TAKE1("GnuTLSRSAFile", mgs_set_rsa_export_file,
-                  NULL,
-                  RSRC_CONF,
-                  "Set the file to read RSA-EXPORT parameters from"),
-    AP_INIT_TAKE1("GnuTLSCertificateFile", mgs_set_cert_file,
-                  NULL,
-                  RSRC_CONF,
-                  "SSL Server X509 Certificate file"),
-    AP_INIT_TAKE1("GnuTLSKeyFile", mgs_set_key_file,
-                  NULL,
-                  RSRC_CONF,
-                  "SSL Server X509 Private Key file"),
-    AP_INIT_TAKE1("GnuTLSX509CertificateFile", mgs_set_cert_file,
-                  NULL,
-                  RSRC_CONF,
-                  "SSL Server X509 Certificate file"),
-    AP_INIT_TAKE1("GnuTLSX509KeyFile", mgs_set_key_file,
-                  NULL,
-                  RSRC_CONF,
-                  "SSL Server X509 Private Key file"),
-    AP_INIT_TAKE1("GnuTLSPGPCertificateFile", mgs_set_pgpcert_file,
-                  NULL,
-                  RSRC_CONF,
-                  "SSL Server PGP Certificate file"),
-    AP_INIT_TAKE1("GnuTLSPGPKeyFile", mgs_set_pgpkey_file,
-                  NULL,
-                  RSRC_CONF,
-                  "SSL Server PGP Private key file"),
+	AP_INIT_TAKE1("GnuTLSClientVerify", mgs_set_client_verify,
+		      NULL,
+		      RSRC_CONF | OR_AUTHCFG,
+		      "Set Verification Requirements of the Client Certificate"),
+	AP_INIT_TAKE1("GnuTLSClientCAFile", mgs_set_client_ca_file,
+		      NULL,
+		      RSRC_CONF,
+		      "Set the CA File to verify Client Certificates"),
+	AP_INIT_TAKE1("GnuTLSX509CAFile", mgs_set_client_ca_file,
+		      NULL,
+		      RSRC_CONF,
+		      "Set the CA File to verify Client Certificates"),
+	AP_INIT_TAKE1("GnuTLSPGPKeyringFile", mgs_set_keyring_file,
+		      NULL,
+		      RSRC_CONF,
+		      "Set the Keyring File to verify Client Certificates"),
+	AP_INIT_TAKE1("GnuTLSDHFile", mgs_set_dh_file,
+		      NULL,
+		      RSRC_CONF,
+		      "Set the file to read Diffie Hellman parameters from"),
+	AP_INIT_TAKE1("GnuTLSRSAFile", mgs_set_rsa_export_file,
+		      NULL,
+		      RSRC_CONF,
+		      "Set the file to read RSA-EXPORT parameters from"),
+	AP_INIT_TAKE1("GnuTLSCertificateFile", mgs_set_cert_file,
+		      NULL,
+		      RSRC_CONF,
+		      "SSL Server X509 Certificate file"),
+	AP_INIT_TAKE1("GnuTLSKeyFile", mgs_set_key_file,
+		      NULL,
+		      RSRC_CONF,
+		      "SSL Server X509 Private Key file"),
+	AP_INIT_TAKE1("GnuTLSX509CertificateFile", mgs_set_cert_file,
+		      NULL,
+		      RSRC_CONF,
+		      "SSL Server X509 Certificate file"),
+	AP_INIT_TAKE1("GnuTLSX509KeyFile", mgs_set_key_file,
+		      NULL,
+		      RSRC_CONF,
+		      "SSL Server X509 Private Key file"),
+	AP_INIT_TAKE1("GnuTLSPGPCertificateFile", mgs_set_pgpcert_file,
+		      NULL,
+		      RSRC_CONF,
+		      "SSL Server PGP Certificate file"),
+	AP_INIT_TAKE1("GnuTLSPGPKeyFile", mgs_set_pgpkey_file,
+		      NULL,
+		      RSRC_CONF,
+		      "SSL Server PGP Private key file"),
 #ifdef ENABLE_SRP
-    AP_INIT_TAKE1("GnuTLSSRPPasswdFile", mgs_set_srp_tpasswd_file,
-                  NULL,
-                  RSRC_CONF,
-                  "SSL Server SRP Password Conf file"),
-    AP_INIT_TAKE1("GnuTLSSRPPasswdConfFile", mgs_set_srp_tpasswd_conf_file,
-                  NULL,
-                  RSRC_CONF,
-                  "SSL Server SRP Parameters file"),
+	AP_INIT_TAKE1("GnuTLSSRPPasswdFile", mgs_set_srp_tpasswd_file,
+		      NULL,
+		      RSRC_CONF,
+		      "SSL Server SRP Password Conf file"),
+	AP_INIT_TAKE1("GnuTLSSRPPasswdConfFile",
+		      mgs_set_srp_tpasswd_conf_file,
+		      NULL,
+		      RSRC_CONF,
+		      "SSL Server SRP Parameters file"),
 #endif
-    AP_INIT_TAKE1("GnuTLSCacheTimeout", mgs_set_cache_timeout,
-                  NULL,
-                  RSRC_CONF,
-                  "Cache Timeout"),
-    AP_INIT_TAKE2("GnuTLSCache", mgs_set_cache,
-                  NULL,
-                  RSRC_CONF,
-                  "Cache Configuration"),
-    AP_INIT_TAKE1("GnuTLSSessionTickets", mgs_set_tickets,
-                  NULL,
-                  RSRC_CONF,
-                  "Session Tickets Configuration"),
-    AP_INIT_RAW_ARGS("GnuTLSPriorities", mgs_set_priorities,
-                  NULL,
-                  RSRC_CONF,
-                  "The priorities to enable (ciphers, Key exchange, macs, compression)."),
-    AP_INIT_TAKE1("GnuTLSEnable", mgs_set_enabled,
-                  NULL,
-                  RSRC_CONF,
-                  "Whether this server has GnuTLS Enabled. Default: Off"),
-    AP_INIT_TAKE1("GnuTLSExportCertificates", mgs_set_export_certificates_enabled,
-                  NULL,
-                  RSRC_CONF,
-                  "Whether to export PEM encoded certificates to CGIs. Default: Off"),
+	AP_INIT_TAKE1("GnuTLSCacheTimeout", mgs_set_cache_timeout,
+		      NULL,
+		      RSRC_CONF,
+		      "Cache Timeout"),
+	AP_INIT_TAKE2("GnuTLSCache", mgs_set_cache,
+		      NULL,
+		      RSRC_CONF,
+		      "Cache Configuration"),
+	AP_INIT_TAKE1("GnuTLSSessionTickets", mgs_set_tickets,
+		      NULL,
+		      RSRC_CONF,
+		      "Session Tickets Configuration"),
+	AP_INIT_RAW_ARGS("GnuTLSPriorities", mgs_set_priorities,
+			 NULL,
+			 RSRC_CONF,
+			 "The priorities to enable (ciphers, Key exchange, macs, compression)."),
+	AP_INIT_TAKE1("GnuTLSEnable", mgs_set_enabled,
+		      NULL,
+		      RSRC_CONF,
+		      "Whether this server has GnuTLS Enabled. Default: Off"),
+	AP_INIT_TAKE1("GnuTLSExportCertificates",
+		      mgs_set_export_certificates_enabled,
+		      NULL,
+		      RSRC_CONF,
+		      "Whether to export PEM encoded certificates to CGIs. Default: Off"),
 #if 0
-    AP_INIT_RAW_ARGS("<GnuTLSRequire", mgs_set_require_section,
-                  NULL,
-                  EXEC_ON_READ|OR_ALL,
-                  "Whether this server has GnuTLS Enabled. Default: Off"),
-    AP_INIT_RAW_ARGS("GnuTLSRequireByteCode", mgs_set_require_bytecode,
-                     NULL,
-                     OR_ALL,
-                     "Internal Command for reading Lua Bytecode."),
+	AP_INIT_RAW_ARGS("<GnuTLSRequire", mgs_set_require_section,
+			 NULL,
+			 EXEC_ON_READ | OR_ALL,
+			 "Whether this server has GnuTLS Enabled. Default: Off"),
+	AP_INIT_RAW_ARGS("GnuTLSRequireByteCode", mgs_set_require_bytecode,
+			 NULL,
+			 OR_ALL,
+			 "Internal Command for reading Lua Bytecode."),
 #endif
-    {NULL}
+	{NULL}
 };
 
 module AP_MODULE_DECLARE_DATA gnutls_module = {
-    STANDARD20_MODULE_STUFF,
-    mgs_config_dir_create,
-    mgs_config_dir_merge,
-    mgs_config_server_create,
-    NULL,
-    mgs_config_cmds,
-    gnutls_hooks
+	STANDARD20_MODULE_STUFF,
+	mgs_config_dir_create,
+	mgs_config_dir_merge,
+	mgs_config_server_create,
+	NULL,
+	mgs_config_cmds,
+	gnutls_hooks
 };
