f84890d19a4a94353ee64a0641007a6a3af1574b
Thomas Klute <thomas2.klute@uni-dortmund.de>
2015-03-31 14:16:39 +0200

    Shared bash file for proxy back end management code
    Futher test cases for proxy operation will need this code as well, so it makes
    sense to keep it in a common file that individual test scripts can parse.

--- test/test-19_TLS_reverse_proxy.bash
+++ test/test-19_TLS_reverse_proxy.bash
@@ -2,48 +2,8 @@
 
 set -e
 
-if [ -z "${BACKEND_HOST}" ]; then
-    export BACKEND_HOST="localhost"
-fi
-if [ -z "${BACKEND_IP}" ]; then
-    export BACKEND_IP="::1"
-fi
-if [ -z "${BACKEND_PORT}" ]; then
-    export BACKEND_PORT="9934"
-fi
-
-function backend_apache
-{
-    dir="${1}"
-    conf="${2}"
-    action="${3}"
-    lockfile="${4}"
-
-    if [ -n "${lockfile}" ]; then
-	flock_cmd="flock -w 10 ${lockfile}"
-    fi
-
-    TEST_NAME="$(basename "${dir}")"
-    (
-	export TEST_NAME
-	export TEST_IP="${BACKEND_IP}"
-	export TEST_PORT="${BACKEND_PORT}"
-	case $action in
-	    start)
-		cd "${dir}"
-		${flock_cmd} \
-		    /usr/sbin/apache2 -f "$(pwd)/${conf}" -k start || return 1
-		;;
-	    stop)
-		cd "${dir}"
-		/usr/sbin/apache2 -f "$(pwd)/${conf}" -k stop || return 1
-		;;
-	esac
-    )
-}
-
 testdir="./tests/19_TLS_reverse_proxy"
-BACKEND_LOCK="$(realpath ${testdir}/backend.lock)"
+. ./proxy_backend.bash
 
 function stop_backend
 {
