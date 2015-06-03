e39119733444d31bc3cfdf820d6d51e6545ee1d0
Thomas Klute <thomas2.klute@uni-dortmund.de>
2015-06-03 07:37:12 +0200

    Update copyright headers for C source

--- src/gnutls_config.c
+++ src/gnutls_config.c
@@ -2,6 +2,7 @@
  *  Copyright 2004-2005 Paul Querna
  *  Copyright 2008 Nikos Mavrogiannopoulos
  *  Copyright 2011 Dash Shendy
+ *  Copyright 2015 Thomas Klute
  *
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
--- src/gnutls_hooks.c
+++ src/gnutls_hooks.c
@@ -3,6 +3,7 @@
  *  Copyright 2008 Nikos Mavrogiannopoulos
  *  Copyright 2011 Dash Shendy
  *  Copyright 2013-2014 Daniel Kahn Gillmor
+ *  Copyright 2015 Thomas Klute
  *
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
