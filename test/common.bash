4d2d182ab3ea1f2cca618a2dbd7fa3fdd619eb67
Thomas Klute <thomas2.klute@uni-dortmund.de>
2016-01-28 21:03:36 +0100

    Test suite: Switch to non-root user namespace before running Apache
    Apache tries to switch permissions to a non-root user when started as
    root. However, inside a namespace with pseudo root access (needed to
    bring up the loopback device of the network namespace) this fails
    because no such user exists inside the namespace. Changing to a
    non-root user namespace beforehand avoids the issue.

--- test/common.bash
+++ test/common.bash
@@ -26,11 +26,18 @@ function wait_pid_gone
 # isolate each testcase inside its own network namespace. Since
 # MGS_NETNS_ACTIVE is used to track status, there's no harm in calling
 # it multiple times (e.g. in the test-* script and runtests).
+#
+# Note that once the network is up, the reexec is wrapped in another
+# user namespace to get rid of pseudo "root" access. The reason for
+# this is that Apache tries to switch permissions to a non-root user
+# when apparently started as root, and fails because no such user
+# exists inside the namespace. Changing to a non-root user beforehand
+# avoids that issue.
 function netns_reexec
 {
     if [ -n "${USE_TEST_NAMESPACE}" ] && [ -z "${MGS_NETNS_ACTIVE}" ]; then
 	exec "${UNSHARE}" --net -r /bin/bash -c \
-	     "export MGS_NETNS_ACTIVE=1; ip link set up lo; exec ${0} ${@}"
+	     "export MGS_NETNS_ACTIVE=1; ip link set up lo; exec ${UNSHARE} --user ${0} ${@}"
     fi
     return 0
 }
