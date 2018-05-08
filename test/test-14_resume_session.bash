13870b7030d01c64fb31b7e0274cd7e3ee0681b0
Fiona Klute <fiona.klute@gmx.de>
2018-05-08 19:53:22 +0200

    test-14_resume_session.bash: Check if the session was resumed successfully
    If resumption is not possible but a second full handshake works,
    gnutls-cli --resume still returns 0. This is correct behavior as a TLS
    client, but a successful resumption test requires resumption.

--- test/test-14_resume_session.bash
+++ test/test-14_resume_session.bash
@@ -1,2 +1,8 @@
 #!/bin/bash
+set -e
 ${srcdir}/runtests t-14
+
+t="$(basename ${0} .bash)"
+output="outputs/${t#test-}.output"
+echo "Checking if the session was resumed successfully..."
+grep "This is a resumed session" "${output}"
