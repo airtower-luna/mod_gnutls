514d4d69915bdb38ddb0a75ac5306317a4ade89b
Fiona Klute <fiona.klute@gmx.de>
2018-09-27 16:05:10 +0200

    Test suite: Two more compatibility fixes for GnuTLS 3.6.4

--- test/test-14_resume_session.bash
+++ test/test-14_resume_session.bash
@@ -5,4 +5,5 @@ ${srcdir}/runtests t-14
 t="$(basename ${0} .bash)"
 output="outputs/${t#test-}.output"
 echo "Checking if the session was resumed successfully..."
-grep "This is a resumed session" "${output}"
+grep "This is a resumed session" "${output}" \
+	|| grep "Resume Handshake was completed"  "${output}"
