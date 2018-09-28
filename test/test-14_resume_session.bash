17545037a5e7431504646a57eeb13e7bcefde1a9
Fiona Klute <fiona.klute@gmx.de>
2018-09-28 20:55:27 +0200

    Test suite: Enable session tickets for session resumption test
    TLS 1.3 supports resumption with session tickets only, so they must be
    enabled here. Commit 514d4d69915bdb38ddb0a75ac5306317a4ade89b added a
    bug that broke the check for resumption, add a comment to avoid
    repeating that mistake.

--- test/test-14_resume_session.bash
+++ test/test-14_resume_session.bash
@@ -5,5 +5,7 @@ ${srcdir}/runtests t-14
 t="$(basename ${0} .bash)"
 output="outputs/${t#test-}.output"
 echo "Checking if the session was resumed successfully..."
-grep "This is a resumed session" "${output}" \
-	|| grep "Resume Handshake was completed"  "${output}"
+# NOTE: The "Resume Handshake was completed" message appears after the
+# second handshake is complete, whether the session has been resumed
+# or not. The following message is required!
+grep "This is a resumed session" "${output}"
