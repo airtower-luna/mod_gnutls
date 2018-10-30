0062eded4d1c06bc26bc9a415a6693f294ff5fd5
Fiona Klute <fiona.klute@gmx.de>
2018-10-30 20:55:46 +0100

    Test for IP-based virtual hosts
    Mostly to make sure the new "same vhost" check for TLS connection and
    request works correctly with IP-based virtual hosts.

---
+++ test/test-30_ip_based_vhosts.bash
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Parse TEST_IP into an array
+declare -a addrs=(${TEST_IP})
+if [ ${#addrs[@]} -lt 2 ]; then
+    echo "This test needs two or more IP addresses in TEST_IP," \
+	 "check ./configure options!"
+    exit 77
+fi
+
+# The two virtual hosts have different IPs, so we can check if
+# selection without SNI works correctly. The request will go to the
+# second one.
+export VHOST1_IP="${addrs[0]}"
+export VHOST2_IP="${addrs[1]}"
+
+# gnutls-cli expects IPv6 addresses without enclosing brackets, remove
+# them
+TARGET_IP="${VHOST2_IP#\[}"
+TARGET_IP="${TARGET_IP%\]}"
+export TARGET_IP
+
+${srcdir}/runtests t-30
