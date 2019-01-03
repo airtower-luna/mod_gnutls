b6c786656cd6032b53b2d9d9a656aa15efa17dc9
Fiona Klute <fiona.klute@gmx.de>
2019-01-03 16:17:55 +0100

    Update copyright headers of files changed this year

--- src/gnutls_hooks.c
+++ src/gnutls_hooks.c
@@ -3,7 +3,7 @@
  *  Copyright 2008, 2014 Nikos Mavrogiannopoulos
  *  Copyright 2011 Dash Shendy
  *  Copyright 2013-2014 Daniel Kahn Gillmor
- *  Copyright 2015-2018 Fiona Klute
+ *  Copyright 2015-2019 Fiona Klute
  *
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
