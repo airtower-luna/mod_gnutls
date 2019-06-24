a8195018bf6c377ee9e6d5b7433ba882d6ec1588
Fiona Klute <fiona.klute@gmx.de>
2019-06-24 20:46:11 +0200

    Test suite: Move template files into per-identity directories
    Also ensure that per-identity build directories exist for VPATH builds.

--- test/test_ca.mk
+++ test/test_ca.mk
@@ -6,7 +6,8 @@
 # General rules to set up a miniature CA & server & client environment
 # for the test suite
 
-%.template: $(srcdir)/%.template.in
+%/template: $(srcdir)/%/template.in %
+	@mkdir -m 0700 -p $(@D)
 	sed s/__HOSTNAME__/$(TEST_HOST)/ < $< > $@
 	sed -i -e "s,__OCSP_URI__,$(OCSP_URI_TEMPLATE)," $@
 	for i in $(patsubst [%],%,$(TEST_IP)); do \
@@ -14,12 +15,12 @@
 	done; \
 	sed -i -e "s,__IP_ADDRESSES__,$${IP_ADDRS#\\n}," $@
 
-%/uid: $(srcdir)/%/uid.in
+%/uid: $(srcdir)/%/uid.in %
+	@mkdir -m 0700 -p $(@D)
 	sed s/__HOSTNAME__/$(TEST_HOST)/ < $< > $@
 
-%/secret.key:
-	mkdir -p $(dir $@)
-	chmod 0700 $(dir $@)
+%/secret.key: %
+	@mkdir -m 0700 -p $(@D)
 	certtool --outfile $@ --generate-privkey
 
 .PRECIOUS: %/secret.key
@@ -55,17 +56,17 @@
 	GNUPGHOME=authority/ $(GPG_FLOCK) gpg --output $@ --armor --export "$$(GNUPGHOME=$(dir $@) gpg --with-colons --list-secret-keys --fingerprint | grep ^fpr: | cut -f 10 -d :)"
 
 # special cases for the authorities' root certs:
-authority/x509.pem: authority.template authority/secret.key
-	certtool --outfile $@ --generate-self-signed --load-privkey authority/secret.key --template authority.template
-rogueca/x509.pem: $(srcdir)/rogueca.template rogueca/secret.key
-	certtool --outfile $@ --generate-self-signed --load-privkey rogueca/secret.key --template $(srcdir)/rogueca.template
+authority/x509.pem: authority/template authority/secret.key
+	certtool --outfile $@ --generate-self-signed --load-privkey authority/secret.key --template authority/template
+rogueca/x509.pem: $(srcdir)/rogueca/template rogueca/secret.key
+	certtool --outfile $@ --generate-self-signed --load-privkey rogueca/secret.key --template $(srcdir)/rogueca/template
 
 # normal case: certificates signed by test CA
-%/x509.pem: %.template %/secret.key authority/secret.key authority/x509.pem
+%/x509.pem: %/template %/secret.key authority/secret.key authority/x509.pem
 	certtool --outfile $@ --generate-certificate --load-ca-certificate authority/x509.pem --load-ca-privkey authority/secret.key --load-privkey $(dir $@)secret.key --template $<
 
 # error case: certificates signed by rogue CA
-rogue%/x509.pem: rogue%.template rogue%/secret.key rogueca/x509.pem
+rogue%/x509.pem: rogue%/template rogue%/secret.key rogueca/x509.pem
 	certtool --outfile $@ --generate-certificate --load-ca-certificate rogueca/x509.pem --load-ca-privkey rogueca/secret.key --load-privkey $(dir $@)secret.key --template $<
 
 %/softhsm.conf: %/secret.key
@@ -91,10 +92,10 @@ rogue%/x509.pem: rogue%.template rogue%/secret.key rogueca/x509.pem
 # revoke the server certificate and check if setting the CRL as
 # GnuTLSProxyCRLFile causes the connection to the back end server to
 # fail.
-%/crl.pem: %/x509.pem ${srcdir}/%-crl.template
+%/crl.pem: %/x509.pem ${srcdir}/%/crl.template
 	certtool --generate-crl \
 		--outfile $@ \
 		--load-ca-privkey authority/secret.key \
 		--load-ca-certificate authority/x509.pem \
 		--load-certificate $< \
-		--template "${srcdir}/$(*)-crl.template"
+		--template "${srcdir}/$(*)/crl.template"
