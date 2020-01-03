f246f00f27c0279053336fe555cb0b1443381f05
Fiona Klute <fiona.klute@gmx.de>
2020-01-03 11:25:41 +0100

    Use post_check hook for test 14_resume_session

--- test/test-14_resume_session.bash
+++ test/test-14_resume_session.bash
@@ -1,12 +1,2 @@
 #!/bin/bash
-set -e
-log="outputs/14_resume_session.log"
-
-${srcdir}/netns_py.bash ${srcdir}/runtest.py --test-number 14 \
-	--log-connection "${log}"
-
-echo "Checking if the session was resumed successfully..."
-# NOTE: The "Resume Handshake was completed" message appears after the
-# second handshake is complete, whether the session has been resumed
-# or not. The following message is required!
-grep "This is a resumed session" "${log}"
+. ${srcdir}/netns_py.bash ${srcdir}/runtest.py --test-number 14
