459a982ebd79bd4a8f22246530378fb03b5ab193
Fiona Klute <fiona.klute@gmx.de>
2020-01-05 12:10:43 +0100

    Include doctest for the mgstest Python package in test suite runs
    This should help with both detecting issues in the package itself, and
    making sure documentation and implementation match.

---
+++ test/doctest-mgstest.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    import doctest
+    import importlib
+    import sys
+    modules = [
+        'mgstest',
+        'mgstest.hooks',
+        'mgstest.http',
+        'mgstest.services',
+        'mgstest.tests'
+    ]
+
+    totals = (0, 0)
+    for m in modules:
+        mod = importlib.import_module(m)
+        result = doctest.testmod(m=mod, verbose=True)
+        totals = tuple(sum(x) for x in zip(totals, result))
+
+    fails, count = totals
+    print(f'Summary over all modules: {fails} out of {count} tests failed.')
+    if fails > 0:
+        sys.exit(1)
