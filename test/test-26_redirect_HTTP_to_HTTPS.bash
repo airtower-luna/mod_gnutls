3871dbdfa26df53520b6cf420b2c4261d21bb74b
Fiona Klute <fiona.klute@gmx.de>
2020-01-08 19:15:20 +0100

    Move TEST_HTTP_PORT definition to Makefile.am with the other ports
    Having all port definitions in one place makes it easier to understand
    the test environment.

--- test/test-26_redirect_HTTP_to_HTTPS.bash
+++ test/test-26_redirect_HTTP_to_HTTPS.bash
@@ -2,8 +2,4 @@
 #
 # This test checks if mod_rewrite rules can detect HTTPS connections
 # with "%{HTTPS}".
-
-: ${TEST_HTTP_PORT:="9935"}
-export TEST_HTTP_PORT
-
 . ${srcdir}/netns_py.bash ${srcdir}/runtest.py --test-number 26
