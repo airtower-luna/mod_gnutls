482bafce97305f491826fa5c661424c74739633d
Fiona Klute <fiona.klute@gmx.de>
2020-01-08 23:22:32 +0100

    Test suite: Remove checks for no longer supported GnuTLS version
    Mod_gnutls now requires 3.6.3, so checking for a gnutls-cli minimum
    version of 3.5.12 has become pointless: The build would fail before
    the tests with an older version.
    
    This was the last used function from common.bash, so delete the file.

--- test/test-32_vhost_SNI_serveralias_mismatch.bash
+++ test/test-32_vhost_SNI_serveralias_mismatch.bash
@@ -1,7 +1,2 @@
 #!/bin/bash
-set -e
-: ${srcdir:="."}
-. ${srcdir}/common.bash
-
-require_gnutls_cli 3.5.12 || (echo "Using --sni-hostname requires gnutls-cli version 3.5.12 or newer"; exit 77)
 . ${srcdir}/netns_py.bash ${srcdir}/runtest.py --test-number 32
