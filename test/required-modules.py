ee6351d00bd82862ab49d3ae272eed107a6f3aa1
Fiona Klute <fiona.klute@gmx.de>
2020-01-30 13:46:37 +0100

    Test suite: Use mod_mpm_event by default
    The "event" MPM module is more reliable, Valgrind tests showed
    occasional mutex errors during shutdown in "worker". The tests still
    fall back on the "worker" MPM if "event" is not available (that was
    the case on Debian/HURD the last time I checked).

--- test/required-modules.py
+++ test/required-modules.py
@@ -3,14 +3,11 @@ import os
 import re
 import shutil
 import subprocess
+from pathlib import Path
 
 required_modules = {'logio', 'unixd', 'log_config'}
 
-apache2 = os.environ.get('APACHE2')
-if not apache2:
-    apache2 = shutil.which('apache2')
-if not apache2:
-    apache2 = shutil.which('httpd')
+apache2 = os.environ['APACHE2']
 
 result = subprocess.run([apache2, '-l'], check=True,
                         stdout=subprocess.PIPE, text=True)
@@ -24,3 +21,12 @@ for line in result.stdout.splitlines():
 
 for mod in (required_modules - built_in_modules):
     print(f'LoadModule\t{mod}_module\t${{AP_LIBEXECDIR}}/mod_{mod}.so')
+
+# select mpm module, list is ordered by preference
+mpm_choices = ['event', 'worker']
+mod_dir = Path(os.environ['AP_LIBEXECDIR'])
+for mpm in mpm_choices:
+    mod = mod_dir.joinpath(f'mod_mpm_{mpm}.so')
+    if mod.exists():
+        print(f'LoadModule\tmpm_{mpm}_module\t{mod!s}')
+        break
