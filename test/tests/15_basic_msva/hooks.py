cbc3477a51bacd86f43bb27877f89773545cec76
Fiona Klute <fiona.klute@gmx.de>
2020-09-23 11:59:39 +0200

    Skip MSVA test if building without MSVA auth support
    Skipping instead of excluding it from the list of tests removes a
    special case, and might allow simplifications in test handling later.

--- test/tests/15_basic_msva/hooks.py
+++ test/tests/15_basic_msva/hooks.py
@@ -1,4 +1,7 @@
 import os
+from unittest import SkipTest
 
 def prepare_env():
+    if 'MSVA_PORT' not in os.environ:
+        raise SkipTest('Build without MSVA support.')
     os.environ['USE_MSVA'] = 'yes'
