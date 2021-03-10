520ab08c7466168e0800784011031b8383727a5b
Fiona Klute <fiona.klute@gmx.de>
2021-03-10 23:54:46 +0100

    Test mgstest.tests.TestRequest using a mocked connection

--- test/mgstest/test_tests.py
+++ test/mgstest/test_tests.py
@@ -1,9 +1,58 @@
 import unittest
+from http import HTTPStatus
+from unittest.mock import Mock, patch
+
 from . import TestExpectationFailed
 from .tests import TestRequest
 
 
+def mock_response(status=HTTPStatus.OK, headers=dict(), body=b''):
+    response = Mock()
+    response.status = status
+    response.reason = status.phrase
+    response.getheaders.return_value = [(k, v) for k, v in headers.items()]
+    response.read.return_value = body
+    return response
+
+
 class TestTestRequest(unittest.TestCase):
+    def test_run(self):
+        """Check that TestRequest matches regular response correctly."""
+        response = mock_response(
+            HTTPStatus.OK, {'X-Required-Header': 'Hi!'}, b'Hello World!\n')
+        conn = Mock()
+        conn.getresponse.return_value = response
+
+        r = TestRequest(path='/test.txt',
+                        expect={'status': 200,
+                                'headers': {'X-Forbidden-Header': None,
+                                            'X-Required-Header': 'Hi!'},
+                                'body': {'contains': 'Hello'}})
+        r.run(conn)
+        conn.request.assert_called_with(
+            'GET', '/test.txt', body=None, headers={})
+
+    def test_run_unexpected_reset(self):
+        """An unexpected exception breaks out of the TestRequest run."""
+        conn = Mock()
+        conn.request.side_effect = ConnectionResetError
+        r = TestRequest(path='/test.txt',
+                        expect={'status': 200})
+        with self.assertRaises(ConnectionResetError):
+            r.run(conn)
+        conn.request.assert_called_with(
+            'GET', '/test.txt', body=None, headers={})
+
+    def test_run_expected_reset(self):
+        """If the TestRequest expects an exception, it gets caught."""
+        conn = Mock()
+        conn.request.side_effect = ConnectionResetError
+        r = TestRequest(path='/test.txt',
+                        expect={'reset': True})
+        r.run(conn)
+        conn.request.assert_called_with(
+            'GET', '/test.txt', body=None, headers={})
+
     def test_check_headers(self):
         r = TestRequest(path='/test.txt',
                         expect={'headers': {'X-Forbidden-Header': None,
