4281e28c4549ce363b32b15baa96c06f57427164
Fiona Klute <fiona.klute@gmx.de>
2024-04-08 01:34:06 +0200

    Remove executable bit & shebang where not needed
    This ensures scripts aren't accidentally called like binaries instead
    of through the interpreter, minimizing the amount of locations where a
    different interpreter location might cause issues.

--- test/doctest-mgstest.py
+++ test/doctest-mgstest.py
@@ -1,4 +1,3 @@
-#!/usr/bin/python3
 import doctest
 import importlib
 import mgstest
--- test/softhsm-init.py
+++ test/softhsm-init.py
@@ -1,6 +1,3 @@
-#!/usr/bin/python3
-# PYTHON_ARGCOMPLETE_OK
-
 # Copyright 2020 Fiona Klute
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
