e3a9c9d50bfac93dbcafafbd9aa9f3e7148394f7
Fiona Klute <fiona.klute@gmx.de>
2024-04-07 23:12:57 +0200

    Run unshare from Python, without Bash in between
    Mostly a tiny optimization, also reduce reliance on a specific shell.

--- test/tap.py
+++ test/tap.py
@@ -1,29 +1,57 @@
-import os
 import asyncio
+import os
+import shlex
+import sys
 from pathlib import Path
 
 srcdir = Path(os.environ['srcdir'])
 testdir = srcdir / 'tests'
 
 
-async def run_test(c, num, name):
-    netns = srcdir / 'netns_py.bash'
-    runtest = srcdir / 'runtest.py'
-    valgrind_args = []
-    if os.environ.get('ENABLE_VALGRIND') == 'true':
-        valgrind_args = [
-            '--valgrind',
-            '--valgrind-suppressions',
-            str(srcdir / 'suppressions.valgrind'),
-        ]
+async def runtest_namespace(*command):
+    """Run the given runtest.py command in a namespace (via unshare),
+    otherwise directly in a fresh Python interpreter instance.
+
+    If a namespace is used, it will have an active loopback
+    interface. Note that "unshare" is called twice: First to create
+    network/IPC/user namespaces with root (so activating loopback
+    works), and then again to create another user namespace so the
+    test does not run as root even in the namespace (otherwise Apache
+    will try to change user and fail).
+
+    """
+    if os.environ.get('USE_TEST_NAMESPACE'):
+        unshare = os.environ['UNSHARE']
+        c = [
+            unshare, '--net', '--ipc', '-r',
+            'sh', '-c',
+            'export MGS_NETNS_ACTIVE=1; ip link set up lo; '
+            f'exec {shlex.quote(unshare)} '
+            f'--user {shlex.quote(sys.executable)} '
+            f'-u {shlex.join(command)}']
+    else:
+        c = [sys.executable, '-u', *command]
 
     s = await asyncio.create_subprocess_exec(
-        str(netns), str(runtest),
-        *valgrind_args,
-        '--test-number', str(num),
+        *c,
         stdout=asyncio.subprocess.PIPE,
         stderr=asyncio.subprocess.PIPE)
     await s.wait()
+    return s
+
+
+async def run_test(c, num, name):
+    runtest = srcdir / 'runtest.py'
+    valgrind_args = ()
+    if os.environ.get('ENABLE_VALGRIND') == 'true':
+        valgrind_args = (
+            '--valgrind',
+            '--valgrind-suppressions',
+            str(srcdir / 'suppressions.valgrind'),
+        )
+
+    s = await runtest_namespace(
+        str(runtest), *valgrind_args, '--test-number', str(num))
     stdout, stderr = await s.communicate()
     with open(Path(f'{num:02}_{name.replace(" ", "_")}.log'), 'wb') as fh:
         fh.write(b'======= stdout =======\n\n')
